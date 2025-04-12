b46f1808e288ca998699255cfcc36696cb58838f
Qaldek <qaldek@gmx.com>
2025-04-12 23:47:06 +0200

    improve: use fieldset htlm tag

--- src/lib/components/snippets/FrameSnippet.svelte
+++ src/lib/components/snippets/FrameSnippet.svelte
@@ -1,9 +1,14 @@
 // TODO : To USE ?
 
-{#snippet FrameSnippet(title = '', class_frame = '', class_title = 'bg-gray-100 text-fluid-sm', children)}
+{#snippet FrameSnippet(
+	title = '',
+	class_frame = '',
+	class_title = 'bg-gray-100 text-fluid-sm',
+	children
+)}
 	<div
 		id="info"
-		class="relative rounded-xl border border-gray-300 shadow p-6 my-2 {class_frame
+		class="relative my-2 rounded-xl border border-gray-300 p-6 shadow {class_frame
 			? class_frame
 			: ''}"
 	>
@@ -11,7 +16,7 @@
 			<p
 				class="{class_title
 					? class_title
-					: ''} px-2 absolute left-2.5 -top-3 font-semibold text-gray-700"
+					: ''} absolute -top-3 left-2.5 px-2 font-semibold text-gray-700"
 			>
 				{title}
 			</p>
