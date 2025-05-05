c65bd59efe6b63fda472c51a3f8de325a9a82092
QaldeK <aldek@vivaldi.net>
2025-05-05 17:07:51 +0200

    Enhance UserEventsCard with status and task updates
    Redesigns the card layout for responsiveness using container
    queries. Adds an event status indicator and displays unassigned
    tasks.
    
    Updates the task management button logic and refactors user
    retrieval to use `userDb`.

--- src/lib/components/UserEventsCard.svelte
+++ src/lib/components/UserEventsCard.svelte
@@ -1,121 +1,154 @@
 <script lang="ts">
-	import { eventState, modalState } from '$lib/shared/states.svelte';
-	import type { EventType } from '$lib/schemas/event.schema';
-	import { lisibleDate, lisibleTime } from '$lib/utils';
-	import { getContext } from 'svelte';
-	import { Pencil, UserMinus } from 'lucide-svelte';
-	import type { UserType } from '$lib/types/types';
-	import { eventsStore } from '$lib/shared/eventsStore.svelte';
+	import { eventState, modalState } from "$lib/shared/states.svelte";
+	import type { EventType } from "$lib/schemas/event.schema";
+	import { lisibleDate, lisibleTime } from "$lib/utils";
+	import { userDb } from "$lib/shared/userDb.svelte";
+	import { Pencil, UserMinus } from "lucide-svelte";
+	import type { UserType } from "$lib/types/types";
+	import { eventsStore } from "$lib/shared/eventsStore.svelte";
 
 	let { event } = $props<{
 		event: EventType;
 	}>();
 
-	// FIXIT: don't use getContext('currentUser') ?
-	let currentUser: UserType = getContext('currentUser');
+	let currentUser: UserType | null = userDb.current;
 
 	const isCurrentUserSubscribed = $derived(() => {
-		return event.organizers?.some((org) => org.id === currentUser.id);
+		return event.organizers?.some((org) => org.id === currentUser?.id);
 	});
 
-	const unsubscribeFromEvent = () => {
+	const eventStatus = $derived.by(() => {
+		if (event.isConfirmed && !event.cancelled) {
+			return { label: "confirmé", labelColor: "text-success", bg: "bg-success/20" };
+		} else if (event.cancelled) {
+			return { label: "annulé", labelColor: "text-error", bg: "bg-error/20" };
+		} else if (!event.isConfirmed) {
+			return {
+				label: "en attente de confirmation",
+				labelColor: "text-warning",
+				bg: "bg-warning/20"
+			};
+		}
+		// 👉 Retourne un objet par défaut si aucune condition n'est remplie
+		return { label: "", labelColor: "", bg: "" };
+	});
+
+	const manageTasks = () => {
 		if (!currentUser) return;
-		// manageTaskSubscription gère la désinscription quand l'utilisateur est déjà inscrit
-		eventsStore.manageTaskSubscription(event, currentUser);
+		eventsStore.requestTaskUpdate({ event: event, user: currentUser });
 	};
 
 	// 👉 Helper pour lister les tâches de l'utilisateur actuel
 	const currentUserTasks = $derived(
-		event.organizers?.find((org) => org.id === currentUser.id)?.tasks ?? []
+		event.organizers?.find((org) => org.id === currentUser?.id)?.tasks ?? []
 	);
 </script>
 
-<div class=" bg-base-100 mb-4 overflow-hidden rounded-lg border shadow-md">
-	<div class="flex h-full flex-col">
-		<div class="p-3">
-			<!-- En-tête -->
-			<div class="flex items-center justify-between gap-4">
-				<div class="grid">
-					<div class="text-fluid-lg font-bold">{event.event_title}</div>
-					<div class="text-fluid-xs mt-1 mb-2">
-						{#each event.categories as category, index (category)}
-							<span class="font-medium uppercase">
-								{category}{index < event.categories.length - 1 ? ', ' : ''}
-							</span>
-						{/each}
-					</div>
-				</div>
-				<div class="text-fluid-sm text-right">
-					{#if event.date_event}
-						<div class="font-medium">{lisibleDate(new Date(event.date_event))}</div>
-						{#if event.time_start && event.time_end}
-							<div class="text-base-content/80">{event.time_start} - {event.time_end}</div>
-						{/if}
-					{:else if event.dates_proposed?.length}
-						<div class="text-warning">Sondage en cours</div>
-					{:else}
-						<div class="text-base-content/70 italic">Date à définir</div>
-					{/if}
-				</div>
-			</div>
-
-			<!-- Organisateurs et tâches -->
-			<div class="my-2 flex flex-wrap items-center gap-2">
-				{#if currentUserTasks.length > 0}
-					<div class="text-fluid-sm">Vos taches:</div>
-					{#each currentUserTasks as taskName (taskName)}
-						<div class="badge badge-primary badge-soft font-semibold">
-							{taskName}
-						</div>
-					{/each}
-				{:else if isCurrentUserSubscribed()}
-					<div class="badge badge-warning badge-soft font-semibold">Aucune tâche assignée</div>
+<div class="@container mb-4 flex h-full flex-col rounded-lg border shadow-md">
+	<!-- En-tête -->
+	<div class="items-top flex gap-4 @max-md:flex-col @md:justify-between">
+		<div
+			class="text-fluid-base @max-md:flex @max-md:flex-wrap @max-md:gap-x-4 @max-md:px-4 @max-md:py-1 @max-md:align-middle @md:order-2 @md:text-right {eventStatus.bg} @max-md:rounded-t-lg @md:self-start @md:rounded-tr-lg @md:rounded-bl-lg @md:py-2 @md:pr-4 @md:pl-8"
+		>
+			{#if event.date_event}
+				<div class="font-medium">{lisibleDate(new Date(event.date_event))}</div>
+				{#if event.time_start && event.time_end}
+					<div class="text-base-content/80">{event.time_start} - {event.time_end}</div>
 				{/if}
-			</div>
-			<div>
-				{#if event.organizers.length > 1}
-					<span>Avec:</span>
-					{#each event.organizers as organizer (organizer.id)}
-						<div
-							class="badge badge-soft badge-accent me-2 font-semibold {organizer.username ===
-								currentUser.username && 'hidden'}"
-						>
-							{organizer.username}
-						</div>
-					{/each}
-				{:else}
-					<span class="text-fluid-sm text-base-content/80"
-						>pas d'autre organisateur·ice pour le moment</span
-					>
-				{/if}
-			</div>
-		</div>
-
-		<div class="mt-auto flex justify-end gap-x-2 border-t px-3 py-1.5">
-			{#if isCurrentUserSubscribed()}
-				<button
-					onclick={unsubscribeFromEvent}
-					class="btn btn-outline btn-compact {currentUserTasks.length > 1
-						? 'btn-accent'
-						: 'btn-error'}"
-				>
-					<UserMinus class="mr-1 h-4 w-4" />
-					{#if currentUserTasks.length > 1}
-						vos tâches
-					{:else}
-						se désinscrire
-					{/if}
-				</button>
+			{:else if event.dates_proposed?.length}
+				<div class="text-warning">Sondage en cours</div>
+			{:else}
+				<div class="text-base-content/70 italic">Date à définir</div>
 			{/if}
-			<button
-				onclick={() => {
-					eventState.is = event;
-					modalState.event = true;
-				}}
-				class="btn btn-compact btn-soft"
-			>
-				<Pencil class="mr-1 h-4 w-4" />
-			</button>
+			<div class="text-fluid-xs font-medium italic {eventStatus.labelColor}">
+				{eventStatus.label}
+			</div>
 		</div>
+		<div class="grid px-4 pb-2 @md:my-4">
+			<div class="text-fluid-lg font-bold">{event.event_title}</div>
+			<div class="text-fluid-base mt-1 mb-2">
+				{#each event.categories as category, index (category)}
+					<span class="font-medium uppercase">
+						{category}{index < event.categories.length - 1 ? ", " : ""}
+					</span>
+				{/each}
+			</div>
+		</div>
+	</div>
+
+	<div class="flex flex-1 flex-col gap-2 p-4">
+		<!-- Organisateurs et tâches -->
+		<div class="my-2 flex flex-wrap items-center gap-2">
+			{#if currentUserTasks.length > 0}
+				<div class="text-fluid-sm">Vos taches:</div>
+				{#each currentUserTasks as taskName (taskName)}
+					<div class="badge badge-primary badge-soft font-semibold">
+						{taskName}
+					</div>
+				{/each}
+			{:else if isCurrentUserSubscribed()}
+				<div class="badge badge-warning badge-soft font-semibold">Aucune tâche assignée</div>
+			{/if}
+		</div>
+		<div>
+			{#if event.organizers.length > 1}
+				<span>Avec:</span>
+				{#each event.organizers as organizer (organizer.id)}
+					<div
+						class="badge badge-soft badge-accent me-2 font-semibold {organizer.username ===
+							currentUser.username && 'hidden'}"
+					>
+						{organizer.username}
+					</div>
+				{/each}
+			{:else}
+				<span class="text-fluid-sm text-base-content/60"
+					>Il n'y a pas d'autre organisateur·ice pour le moment</span
+				>
+			{/if}
+		</div>
+		{#if Array.isArray(event.tasks) && event.tasks.length > 1}
+			{@const assignedTasks = event.organizers?.flatMap((org) => org.tasks || []) || []}
+			{@const unassignedTasks = event.tasks.filter((task) => !assignedTasks.includes(task.name))}
+			{#if unassignedTasks.length > 0}
+				<div class="ms-auto mt-auto flex flex-wrap gap-1 pt-4">
+					<span class="text-fluid-sm text-base-content/60">Tâches non attribuées:</span>
+					{#each unassignedTasks as task, i (i)}
+						<span title={task.description} class="badge badge-soft badge-sm me-1 mb-1"
+							>{task.name}</span
+						>
+					{/each}
+				</div>
+			{:else}
+				<div class="text-fluid-sm text-base-content/60 ms-auto mt-auto flex flex-wrap gap-1 pt-4">
+					Toutes les tâches sont attribuées
+				</div>
+			{/if}
+		{/if}
+	</div>
+
+	<div class="mt-auto flex justify-end gap-x-2 border-t px-3 py-1.5">
+		{#if isCurrentUserSubscribed()}
+			<button
+				onclick={manageTasks}
+				class="btn btn-outline btn-compact {event.tasks.length > 1 ? 'btn-accent' : 'btn-error'}"
+			>
+				<UserMinus class="mr-1 h-4 w-4" />
+				{#if event.tasks.length > 1}
+					vos tâches
+				{:else}
+					se désinscrire
+				{/if}
+			</button>
+		{/if}
+		<button
+			onclick={() => {
+				eventState.is = event;
+				modalState.event = true;
+			}}
+			class="btn btn-compact btn-square btn-soft"
+		>
+			<Pencil size={20} />
+		</button>
 	</div>
 </div>
