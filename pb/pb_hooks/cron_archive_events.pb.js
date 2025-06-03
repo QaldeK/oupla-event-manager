7108fb6ac41008484c102aaa174de4b3dd6d30bf
Qaldek <qaldek@gmx.com>
2025-06-03 19:05:06 +0200

    bun update + shadcn update + prettier

--- pb/pb_hooks/cron_archive_events.pb.js
+++ pb/pb_hooks/cron_archive_events.pb.js
@@ -1,20 +1,20 @@
 // Définition du cron pour l'archivage des événements
 // S'exécute tous les jours à minuit : "0 2 1 * *" / valeur de test : * * * * *
-cronAdd('archive_events', '0 2 1 * *', async () => {
+cronAdd("archive_events", "0 2 1 * *", async () => {
 	console.log(`👉 [${new Date().toISOString()}] Archivage des événements en cours...`);
 
-	const today = new Date().toISOString().split('T')[0];
+	const today = new Date().toISOString().split("T")[0];
 
 	try {
 		// Récupérer les événements passés
 		const pastEvents = await $app.findRecordsByFilter(
-			'events',
+			"events",
 			`date_event != '' && date_event < '${today}'`
 		);
 
 		// Utiliser une transaction pour garantir l'intégrité des données
 		$app.runInTransaction((txApp) => {
-			const eventsPastCollection = txApp.findCollectionByNameOrId('events_past');
+			const eventsPastCollection = txApp.findCollectionByNameOrId("events_past");
 
 			for (const event of pastEvents) {
 				// Créer un nouvel enregistrement dans events_past
@@ -22,18 +22,18 @@ cronAdd('archive_events', '0 2 1 * *', async () => {
 
 				// Copier uniquement les champs nécessaires
 				const fieldsToArchive = [
-					'space',
-					'event_title',
-					'date_event',
-					'isRecurrent',
-					'masterRecurrentId',
-					'created_by',
-					'organizer',
-					'isPublic',
-					'isConfirmed',
-					'start_public',
-					'desc_public',
-					'categories'
+					"space",
+					"event_title",
+					"date_event",
+					"isRecurrent",
+					"masterRecurrentId",
+					"created_by",
+					"organizer",
+					"isPublic",
+					"isConfirmed",
+					"start_public",
+					"desc_public",
+					"categories"
 				];
 
 				fieldsToArchive.forEach((field) => {
